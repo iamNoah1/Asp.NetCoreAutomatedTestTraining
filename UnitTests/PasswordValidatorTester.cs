ae631bbfece31c27f9d86f34c7b094d75410e355
Noah Ispas <noi@dataone.de>
2020-06-24 16:39:02 +0200

    implemented solution for step2

--- UnitTests/PasswordValidatorTester.cs
+++ UnitTests/PasswordValidatorTester.cs
@@ -1,3 +1,4 @@
+using BusinessLogic;
 using System;
 using Xunit;
 
@@ -10,5 +11,61 @@ namespace UnitTests
         {
             Assert.True(true);
         }
+
+        [Fact]
+        public void PasswordValidatorShouldThrowExpectionIfPasswordIsNull()
+        {
+            PasswordValidator unitUnderTest = new PasswordValidator();
+            string passwordToCheck = null;
+
+            Action action = () => unitUnderTest.IsValid(passwordToCheck);
+
+            Assert.Throws<Exception>(action);
+        }
+
+        [Fact]
+        public void PasswordValidatorShouldThrowExpectionIfPasswordIsEmpty()
+        {
+            PasswordValidator unitUnderTest = new PasswordValidator();
+            string passwordToCheck = "";
+
+            Action action = () => unitUnderTest.IsValid(passwordToCheck);
+
+            Assert.Throws<Exception>(action);
+        }
+
+        [Fact]
+        public void PasswordValidatorShouldReturnFalseIfPasswordLengthIsLessThan8()
+        {
+            PasswordValidator unitUnderTest = new PasswordValidator();
+            string passwordToCheck = "1234567";
+
+            bool result = unitUnderTest.IsValid(passwordToCheck);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void PasswordValidatorShouldReturnFalseIfPasswordLengthIsMoreThan20()
+        {
+            PasswordValidator unitUnderTest = new PasswordValidator();
+            string passwordToCheck = "1234567891234567890123";
+
+            bool result = unitUnderTest.IsValid(passwordToCheck);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void PasswordValidatorShouldReturnTrueIfPasswordLengthIsBetween8And20()
+        {
+            PasswordValidator unitUnderTest = new PasswordValidator();
+            string passwordToCheck = "1234567891234";
+
+            bool result = unitUnderTest.IsValid(passwordToCheck);
+
+            Assert.True(result);
+        }
     }
+
 }
