6b603d5392b4c9f4f9950da0019bbd58f6021b3d
Noah Ispas <noah.ispas@dataone.de>
2020-07-12 20:32:12 +0200

    adjust app startup to run as web api

--- AutomatedTestTraining/Program.cs
+++ AutomatedTestTraining/Program.cs
@@ -1,5 +1,6 @@
 using BusinessLogic;
-using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace AutomatedTestTraining
 {
@@ -9,44 +10,14 @@ namespace AutomatedTestTraining
 
         public static void Main(string[] args)
         {
-            Console.WriteLine("Playing around with Password Validation");
-
-            string password = null;
-            bool valid = false;
-
-            try
-            {
-                validator.IsValid(password);
-            } catch(Exception e)
-            {
-                Console.WriteLine($"Error while validating password: {password}, Error: {e.Message}");
-            }
-
-            password = "";
-
-            try
-            {
-                validator.IsValid(password);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Error while validating password: {password}, Error: {e.Message}");
-            }
-
-            password = "1234567";
-            valid = validator.IsValid(password);
-
-            Console.WriteLine($"Password {password} is valid: {valid}");
-
-            password = "123456789";
-            valid = validator.IsValid(password);
-
-            Console.WriteLine($"Password {password} is valid: {valid}");
-
-            password = "123456789012345691234567890";
-            valid = validator.IsValid(password);
-
-            Console.WriteLine($"Password {password} is valid: {valid}");
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
---
+++ AutomatedTestTraining/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BusinessLogic
+{
+    public class Startup
+    {
+        public Startup()
+        {
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddMvc().AddNewtonsoftJson();
+            services.AddSingleton<UserService, UserService>();
+            services.AddSingleton<PasswordValidator, PasswordValidator>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
