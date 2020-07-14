de4d36fcc68a8758767695504da099a00d42c369
Noah Ispas <noah.ispas@dataone.de>
2020-07-14 09:49:51 +0200

    intermediate

---
+++ UnitTests/UserControllerTester.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xunit;
+
+namespace UnitTests
+{
+    public class UserControllerTester
+    {
+        public UserControllerTester()
+        {
+        }
+
+        [Fact]
+        public void AddUserShouldReturn400IfBodyIsEmpty()
+        {
+
+        }
+
+        [Fact]
+        public void AddUserShouldReturn400IfThereIsNoBody()
+        {
+
+        }
+
+        [Fact]
+        public void AddUserShouldReturn400IfPasswordIsInvalid()
+        {
+
+        }
+
+        [Fact]
+        public void AddUserShouldReturn200IfUserWasAddedSuccessfully()
+        {
+
+        }
+
+        [Fact]
+        public void AddUserShouldReturn500IfAddingUserFailed()
+        {
+
+        }
+    }
+}
